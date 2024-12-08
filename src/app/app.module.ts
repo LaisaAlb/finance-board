67cd6c45dc5a7d9b956c3d98fc346b9330616a8e
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2024-12-08 00:40:44 -0300

    Base structure

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BalanceCardComponent,
+    CreditCardComponent,
+    DebitCardComponent,
+    ClientInfosCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
